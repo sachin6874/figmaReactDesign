72f664df798c8acff1bab31ee4d3b6b02de4d6f4
Sachin6874 <Sachintanwar.aaa@gmail.com@gmail.com>
2022-08-06 23:08:11 +0530

    responsive commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import react from "react"
 import ReactDom from "react-dom"
 import App from "./App.js"
 import "./style.css"
-
+import "./responsive.css"
 
 ReactDom.render(<App/>,document.getElementById("root"))
